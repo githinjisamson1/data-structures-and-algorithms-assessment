e1e00dc1d82974f967e8cc269a0cc4e6e23913cb
githinjisamson1 <githinjisamson148@gmail.com>
2023-12-07 18:32:02 +0300

    Complete sequences

---
+++ lib/sequences.py
@@ -0,0 +1,12 @@
+
+def removeDuplicates(input):
+    # convert to set/contains no duplicates
+    setWithNoDuplicates = set(input)
+
+    # convert back to list
+    return list(setWithNoDuplicates)
+
+
+input_sequence = [2, 3, 2, 4, 5, 3, 6, 7, 5]
+result = removeDuplicates(input_sequence)
+print(result)  # Output: [2, 3, 4, 5, 6, 7]
